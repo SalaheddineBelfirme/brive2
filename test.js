48f3ff5b6fcfe35327128ce46928a69981702a5b
salaheddien <belfirmesalaheddine@gmail.com>
2022-10-25 09:45:42 +0100

    changer d image

---
+++ test.js
@@ -0,0 +1,7 @@
+var change_color=document.getElementById("b1");
+var p=document.getElementById("p1");
+change_color.addEventListener("click",function (){
+    var titre=document.getElementById("titre");
+    titre.style.color="red";  
+
+});
